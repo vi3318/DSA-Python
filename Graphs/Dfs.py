72f9b73d79c80d873ec231682d250c247d737f76
vi3318 <vihaan.dharia039@nmims.edu.in>
2026-01-21 18:24:16 +0530

    updates6

--- Graphs/Dfs.py
+++ Graphs/Dfs.py
@@ -1,1 +1,31 @@
 # Dfs for graphs
+def dfs(node, result,visited, adj):
+    visited[node] = 1
+    result.append(node)
+    for neighbor in adj[node]:
+        if visited[neighbor] == 0:
+            dfs(neighbor, result, visited, adj)
+
+n = 9
+adjencyList = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3],
+    [3],
+    [7],
+    [2, 6, 8],
+    [1, 7],
+    []
+]
+
+visited = [0] * (n + 1)
+result = []
+dfs(1, result, visited, adjencyList)
+print(result)  # Output: [1, 2, 7, 6, 8, 3, 4, 5]
+
+# TC O(N) + O(E) ~ O(N + E)
+# N for nodes, E for edges
+# SC O(N) + O(N) ~ O(N)
+# N for visited array, N for recursion stack in worst case
--- LinkedList/DllImplementation.py
+++ LinkedList/DllImplementation.py
@@ -1,1 +1,106 @@
 # Doubly linked list implementation
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+class DllNode:
+    def __init__(self):
+        self.head = None
+       
+    
+    def insertAtHead(self, val):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev= new_node
+            self.head = new_node
+            # TC O(1)
+            # SC O(1)
+    
+    def append(self,val):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+            new_node.prev = current
+            # TC O(N)
+            # SC O(1)
+    def insertBetween(self,val,pos):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            count = 0
+            while current and count < pos:
+                current = current.next
+                count += 1
+            if current:
+                new_node.prev = current.prev
+                new_node.next = current
+                if current.prev:
+                    current.prev.next = new_node
+                current.prev = new_node
+                if pos == 0:
+                    self.head = new_node
+            # TC O(N)
+            # SC O(1)
+    def display(self):
+        current = self.head
+        while current:
+            print(current.val, end=" ")
+            current = current.next
+        print()
+        # TC O(N)
+        # SC O(1)
+    def deleteHead(self):
+        if not self.head:
+            return
+        if not self.head.next:
+            self.head = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+        # TC O(1)
+        # SC O(1)
+    def deleteTail(self):
+        if not self.head:
+            return
+        if not self.head.next:
+            self.head = None
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.prev.next = None
+        # TC O(N)
+        # SC O(1)
+        def deleteAtPosition(self,pos):
+            if not self.head:
+                return
+            if pos == 0:
+                self.deleteHead()
+                return
+            current = self.head
+            count = 0
+            while current and count < pos:
+                current = current.next
+                count += 1
+            if current:
+                if current.next:
+                    current.next.prev = current.prev
+                if current.prev:
+                    current.prev.next = current.next
+
+
+    
+
+dll = DllNode()
+dll.insertAtHead(10)
